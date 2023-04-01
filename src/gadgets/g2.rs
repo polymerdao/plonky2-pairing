6818bfcdf22f73ceec0f4eb5162a2a5f21d15df3
Sai Deng <dengsai@hotmail.com>
2023-03-31 19:47:52 -0700

    fix a bug

--- src/gadgets/g2.rs
+++ src/gadgets/g2.rs
@@ -426,7 +426,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilderCurveG2<F, D>
         }
 
         let q1 = self.mul_by_q::<C, FF>(&p);
-        let mul_by_q = self.mul_by_q::<C, FF>(&p);
+        let mul_by_q = self.mul_by_q::<C, FF>(&q1);
         let q2 = self.curve_neg_g2::<C, FF>(&mul_by_q);
 
         let (r0, coeff) = self.mixed_addition_step_for_flipped_miller_loop::<C, FF>(&r, &q1);
@@ -958,60 +958,60 @@ mod tests {
         builder.connect_nonnative_ext2(&coeff_expected_1.ell_0, &g_precomputed.coeffs[1].ell_0);
         builder.connect_nonnative_ext2(&coeff_expected_1.ell_vw, &g_precomputed.coeffs[1].ell_vw);
         builder.connect_nonnative_ext2(&coeff_expected_1.ell_vv, &g_precomputed.coeffs[1].ell_vv);
-        //
-        // let coeff_expected_101 = EllCoefficientsTarget {
-        //     ell_0: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
-        //         Bn128Base([
-        //             17085172404633693200,
-        //             10630938209732224048,
-        //             1371020573765003243,
-        //             1028232206131360563,
-        //         ]),
-        //         Bn128Base([
-        //             12232876582152898836,
-        //             767314283470872833,
-        //             15772841601744581597,
-        //             128106244370315677,
-        //         ]),
-        //     ])),
-        //     ell_vw: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
-        //         Bn128Base([
-        //             13268268652402041517,
-        //             413782302065018431,
-        //             10641674370522314647,
-        //             1317742553538815291,
-        //         ]),
-        //         Bn128Base([
-        //             7340261005993041334,
-        //             12959011140836651059,
-        //             6312169946649546817,
-        //             2020346702179008593,
-        //         ]),
-        //     ])),
-        //     ell_vv: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
-        //         Bn128Base([
-        //             17214549829497107128,
-        //             6367600020633491639,
-        //             4118147622849011930,
-        //             2701443844605818879,
-        //         ]),
-        //         Bn128Base([
-        //             9678615892898801219,
-        //             15105284796503706066,
-        //             15218042156631491887,
-        //             2386565019544464078,
-        //         ]),
-        //     ])),
-        // };
-        // builder.connect_nonnative_ext2(&coeff_expected_101.ell_0, &g_precomputed.coeffs[101].ell_0);
-        // builder.connect_nonnative_ext2(
-        //     &coeff_expected_101.ell_vw,
-        //     &g_precomputed.coeffs[101].ell_vw,
-        // );
-        // builder.connect_nonnative_ext2(
-        //     &coeff_expected_101.ell_vv,
-        //     &g_precomputed.coeffs[101].ell_vv,
-        // );
+
+        let coeff_expected_101 = EllCoefficientsTarget {
+            ell_0: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
+                Bn128Base([
+                    17085172404633693200,
+                    10630938209732224048,
+                    1371020573765003243,
+                    1028232206131360563,
+                ]),
+                Bn128Base([
+                    12232876582152898836,
+                    767314283470872833,
+                    15772841601744581597,
+                    128106244370315677,
+                ]),
+            ])),
+            ell_vw: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
+                Bn128Base([
+                    13268268652402041517,
+                    413782302065018431,
+                    10641674370522314647,
+                    1317742553538815291,
+                ]),
+                Bn128Base([
+                    7340261005993041334,
+                    12959011140836651059,
+                    6312169946649546817,
+                    2020346702179008593,
+                ]),
+            ])),
+            ell_vv: builder.constant_nonnative_ext2(QuadraticExtension::<Bn128Base>([
+                Bn128Base([
+                    17214549829497107128,
+                    6367600020633491639,
+                    4118147622849011930,
+                    2701443844605818879,
+                ]),
+                Bn128Base([
+                    9678615892898801219,
+                    15105284796503706066,
+                    15218042156631491887,
+                    2386565019544464078,
+                ]),
+            ])),
+        };
+        builder.connect_nonnative_ext2(&coeff_expected_101.ell_0, &g_precomputed.coeffs[101].ell_0);
+        builder.connect_nonnative_ext2(
+            &coeff_expected_101.ell_vw,
+            &g_precomputed.coeffs[101].ell_vw,
+        );
+        builder.connect_nonnative_ext2(
+            &coeff_expected_101.ell_vv,
+            &g_precomputed.coeffs[101].ell_vv,
+        );
 
         assert_eq!(g_precomputed.coeffs.len(), 102);
 
